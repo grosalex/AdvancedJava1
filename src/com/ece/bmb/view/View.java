15901e86cfca62cc57317593afcd62bdc085464e
stephane <stephane.bolu@gmail.com>
2015-01-22 16:42:34 +0100

    settins of the view

--- src/com/ece/bmb/view/View.java
+++ src/com/ece/bmb/view/View.java
@@ -1,6 +1,13 @@
 package com.ece.bmb.view;
 
+import fr.ece.fauberte.model.Student;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -19,6 +26,20 @@ public class View{
 		
 		
 		VBox vb = new VBox();
+		HBox hb = new HBox();
+		StackPane root = new StackPane();
+		
+		TextField url = new TextField();
+		Button trace = new Button("Start Traceroute");
+		trace.setOnAction(new EventHandler<ActionEvent>() {		 
+	          @Override
+	          public void handle(ActionEvent event) {
+
+	          }
+	      });
+		
+		hb.getChildren().addAll(url,trace);
+		vb.getChildren().addAll(hb,root);
 		
 		primaryStage.setScene(new Scene(vb, 300, 250));
         primaryStage.show();
