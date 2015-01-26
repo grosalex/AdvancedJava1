8cf34e6f0e17def976072b11b41ef6f5b8da126c
Willy91 <williamarques91@gmail.com>
2015-01-26 15:50:06 +0100

    os detection

--- src/com/ece/bmb/view/View.java
+++ src/com/ece/bmb/view/View.java
@@ -18,6 +18,7 @@ public class View{
 	
 	private Stage primaryStage;
 	private Process processGraph;
+	private String OS;
 
 	Process proc;
 	
@@ -26,7 +27,15 @@ public class View{
 	
 	public View(Stage primaryStage){
 		this.primaryStage=primaryStage;
+		OS = System.getProperty("os.name");
+		
 		try {
+			if(isWindows()) {
+				
+			}
+			if(isUnix()) {
+				
+			}
 			processGraph = Runtime.getRuntime().exec("dot -Tpng dotfile.dot -o graph.png");
 			processGraph.waitFor();
 		} catch (Exception e) {
@@ -59,5 +68,13 @@ public class View{
 		
 		primaryStage.setScene(new Scene(vb, 300, 250));
         primaryStage.show();
+	}
+	
+	public boolean isWindows() {
+		return (OS.indexOf("win") >= 0);
+	}
+	
+	public boolean isUnix() {
+		return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 );
 	}
 }
