c837a3f489cc6d95bb84d678c843741079bd141d
stephane <stephane.bolu@gmail.com>
2015-01-27 14:46:15 +0100

    add menu bar + save view

--- src/com/ece/bmb/view/View.java
+++ src/com/ece/bmb/view/View.java
@@ -4,16 +4,17 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
@@ -45,18 +46,32 @@ public class View{
 	
 
 	public void start() {
-		primaryStage.setTitle("Traceroute"); 
+		primaryStage.setTitle("Traceroute");
+		
+		MenuBar menuBar = new MenuBar();
+		Menu menuHelp = new Menu("Help");
+		menuBar.getMenus().addAll(menuHelp);
 		
 		Image image = new Image("file:graph.png");
 		ImageView imageView1 = new ImageView();
         imageView1.setImage(image);
 		
-		VBox vb = new VBox();
-		HBox hb = new HBox();
+		Scene vb = new Scene(new VBox(),300,300);
+		vb.setFill(Color.OLDLACE);
+		HBox hb1 = new HBox();
+		HBox hb2 = new HBox();
 		ScrollPane sp = new ScrollPane();
 		
 		TextField url = new TextField();
+		TextField name_save = new TextField();
 		Button trace = new Button("Start Traceroute");
+		trace.setOnAction(new EventHandler<ActionEvent>() {		 
+	          @Override
+	          public void handle(ActionEvent event) {
+
+	          }
+	    });
+		Button save = new Button("Save Graphic");
 		trace.setOnAction(new EventHandler<ActionEvent>() {		 
 	          @Override
 	          public void handle(ActionEvent event) {
@@ -66,10 +81,13 @@ public class View{
 		
 		
         sp.setContent(imageView1);
-		hb.getChildren().addAll(url,trace);
-		vb.getChildren().addAll(hb,sp);
+		hb1.getChildren().addAll(url,trace);
+		hb2.getChildren().addAll(name_save,save);
+		((VBox) vb.getRoot()).getChildren().addAll(menuBar,hb1,sp,hb2);
+
 		
-		primaryStage.setScene(new Scene(vb, 400, 400));
+		
+		primaryStage.setScene(vb);
         primaryStage.show();
 	}
 	
